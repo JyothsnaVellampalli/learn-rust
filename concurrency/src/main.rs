44f1f16d7cb46b777916643ed5561b48570fbd4c
RZ-Jyothsna <jyothsna.vellampalli@roanuz.com>
2025-06-07 16:54:31 +0530

    Updated concurrency.

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -37,43 +37,129 @@
 // Ideal for IO-bound tasks
 // code looks like synchronous code but runs asynchronously(concurrently).
 
-// trait Future {
-//     type Output;
-//     fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+
+// Creating Threads
+
+// use std::thread;
+
+// fn main() {
+//     // we want main thread to wait for the spawned thread finish the execution, this
+//     // can be achieved by using JOIN HANDLES.
+//     let handle = thread::spawn(|| {
+//         for i in 1..10 {
+//             println!("in spawned thread: {}", i);
+//         }
+//     });
+
+//     for i in 1..10 {
+//         println!("in main thread: {}", i);
+//     }
+
+//     // if the thread panics,join method wil return a error type.
+//     handle.join().unwrap();
+
+//     // Its upto OS how to schedule threads, this is called "Premptive Scheduling".
 // }
 
-// Future does nothing till it is polled.
-// Full is driven to completion by awaiting or giving it to an executor.
+// Moving valus in to threads 
+// Use move keyword to move values into the thread, as we can't know when the thread  will
+// finish execution, by that time, the main prog can drop the value thread is referring to.
+// so, we need to move values to thread and give ownership to the thread.
 
-// enum Poll<T> {
-//     Ready(T),
-//     Pending,
-// } 
+// Messaging among threads
+// MPSC: Multiple Producer Single Consumer
 
-// fn my_function() -> impl Future<Output = ()> {
-//     println!("I am an async function!");
+// use std::{thread, sync::mpsc};
+
+// fn main() {
+//     let (tx, rx) = mpsc::channel();
+
+//     let sentences = vec![
+//         String::from("Hello, world!"),
+//         String::from("Rust is awesome!"),
+//         String::from("Concurrency is powerful!"),
+//     ];
+
+//     for sentence in sentences {
+//         // cloning the sender to move to each thread;
+//         let tx_clone = tx.clone();
+
+//         thread::spawn(move || {
+//             let reversed = sentence.chars().rev().collect::<String>();
+//             // send the reversed string to the channel
+//             tx_clone.send(reversed).unwrap();
+//         });
+//     }
+
+//     drop(tx); // Close(drops) the sender to signal no more messages will be sent.
+
+//     // recv() method waits for single message,
+//     // and blocks the current thread until all senders are dropped.
+//     // rx.recv().expect("Failed to receive message");
+
+//     for sentence in rx {
+//         println!("Received: {}", sentence);
+//     }
 // }
 
-async fn my_function() {
-    println!("I am an async function!");
-    let s1 = read_from_db().await;
-    println!("first, {}", s1);
-    let s2 = read_from_db().await;
-    println!("second, {}", s2);
+
+// To share state across threads,
+// we use mutex = Mutual exclusion
+// Mutex allows only one thread to access the data at a time.
+// we have lock and unlock to allow only one thread to mutate the data at a time.
+// when lock goes out of scope, it automatically unlocks the mutex.
+
+use std::{thread, sync::{Mutex, Arc}};
+
+struct Database {
+    connections: Vec<u32>
 }
 
-async fn read_from_db() -> String {
-    String::from("hello from DB")
+impl Database {
+    fn new() -> Self {
+        Database {
+            connections: vec![]
+        }
+    }
+
+    fn connect(&mut self, id: u32) {
+        self.connections.push(id);
+    }
 }
 
-// awiat can be used in future inside another future(async function inside another async function)
-// for topmost future(in main, ..), we need manual polling = Runtime or executor.
-// Rust does not have built in async runtime
-// tokio - most popular async runtime library
+fn main() {
+    // to share db ownership across threads
+    // (if db moved in to one thread, db can't ne accessed by other threads),
+    // we need ARC(Atomic reference counting) smart pointer (RC is restricted to single thread).
+    let db = Arc::new(Mutex::new(Database::new()));
 
-// attribute macro
-#[tokio::main]
-async fn main() {
-    let res = my_function().await;
-    println!("Rust");
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let db = Arc::clone(&db);
+
+        let handle = thread::spawn(move || {
+            let mut db_lock = db.lock().unwrap();
+            // calling connect method in db impl.
+            db_lock.connect(i);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Database connections: {:?}", db.lock().unwrap().connections);
 }
+
+// Send trait used to send its references across threads, if a type implements Send trait,
+// it mean that developers are promising compiler that the type is safe,
+// as the send trait is unsafe and can't be checked during compile time.
+
+// RC smart pointer does not implement send, so we use Arc smart pointer.
+// RC and RefCell does not implement Sync trait, mutex implements Sync trait,
+// so we can use Arc<Mutex<T>> to share data across threads.
+
+
