cb06dabfc7784c738030670b2fb52a30fbc6b91f
RZ-Jyothsna <jyothsna.vellampalli@roanuz.com>
2025-06-12 17:07:11 +0530

    Updated async-await.

--- async-await/src/main.rs
+++ async-await/src/main.rs
@@ -1,11 +1,15 @@
-// async-await syntax allowa to write functions, closures and blocks that can pause execution
+// async-await syntax allows to write functions, closures and blocks that can pause execution
 // and yield control back to the runtime.
+// typically these pauses are to way for IO.
 
 
 // async under the hood
 // Future a state machine which will be polled, to check if it can return a value.
 // if the poll fn returns "Ready", a call back function(wake) is called
+
+// Futures are lazy, means they do nothing till they are polled and it is zero cost abstraction.
 // Adding await to async function, will draw the Future to completion.
+
 // trait Future {
 //     type Output;
 //     fn poll(&mut slef, wake: fn()) -> Poll<Self::Output>
@@ -16,13 +20,13 @@
 //     Pending
 // }
 
-// aync functions are the functions that returns something which implements Future trait.
+// async functions are the functions that returns something which implements Future trait.
 // fn my_function() -> impl Future<output = ()> {
 //     println!("I am async function")
 // }
 
-async my_function() {
-    println!("I am async function")
+async fn my_function() {
+    println!("I am async function");
     let s1 = read_from_db().await;
     println!("First: {}", s1);
 
@@ -34,7 +38,13 @@ async fn read_from_db() -> String {
     "DB Result".to_owned()
 }
 
-fn main() {
+// Main is not allowed to be async, need a runtime to run async code for top level function.
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
     my_function().await;
 }
+
+// Futures can be drawn to completion br 2 ways:
+// 1. Using await => only works inside another future(async function)
+// 2. Manually polling until it is ready. => for a topmost code, this wil be done by runtime.
